df83da19a3413f247813212c7fee2bfc8ce3ce64
SergeyGers <gers7777@gmail.com>
2023-05-19 15:04:06 +0300

    Home

--- app/src/main/java/com/example/myreaderapp/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/myreaderapp/screens/home/ReaderHomeScreen.kt
@@ -2,9 +2,12 @@ package com.example.myreaderapp.screens.home
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.FloatingActionButton
@@ -26,20 +29,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myreaderapp.ReaderApplication
+import com.example.myreaderapp.model.MBook
 import com.example.myreaderapp.navigation.ReaderScreens
 import com.google.firebase.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.rpc.RetryInfo
 
+
+@Preview
 @Composable
-fun Home(navController: NavController){
+fun Home(navController: NavController = NavController(LocalContext.current)){
     Scaffold(topBar = {
                       ReaderAppBar(title = "MY BOOK", navController = navController)
 
@@ -53,7 +63,21 @@ fun Home(navController: NavController){
 
         Surface(modifier = Modifier.fillMaxSize()) {
                 //home content
+            HomeContent(navController)
         }
+    }
+
+
+}
+
+@Composable
+fun HomeContent(navController: NavController){
+    Column(Modifier.padding(2.dp), verticalArrangement = Arrangement.SpaceEvenly) {
+        Row(modifier = Modifier.align(alignment = Alignment.Start)) {
+            TitleSection(label = "Your reading \n "+" activity right now")
+            
+        }
+        
     }
 
 
@@ -92,13 +116,32 @@ fun ReaderAppBar(
                       }
                   }) {
                       Icon(imageVector = Icons.Filled.Logout,
-                          contentDescription = "Logout")
+                          contentDescription = "Logout",
+                          tint = Color.Green.copy(alpha = 0.4f)  )
 
                   }
         },
         backgroundColor = Color.Transparent,
         elevation = 0.dp)
         
+
+
+}
+
+@Composable
+fun TitleSection(modifier: Modifier = Modifier, label:String){
+    Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
+        Column {
+            Text(text = label, fontSize = 19.sp, fontStyle = FontStyle.Normal, textAlign = TextAlign.Left)
+        }
+    }
+}
+
+
+
+@Composable
+fun ReadingRightNowArea(books: List<MBook>,
+                        navController: NavController) {
 
 
 }
