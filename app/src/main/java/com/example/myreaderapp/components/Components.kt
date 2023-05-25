161dd11bfc4d8ab2a5f621fb9cd8723f1d43d19c
Veronika Kovalev <vika.kovaliov@gmail.com>
2023-05-25 19:41:59 +0300

    Note TextField and Rating bar added

--- app/src/main/java/com/example/myreaderapp/components/Components.kt
+++ app/src/main/java/com/example/myreaderapp/components/Components.kt
@@ -1,7 +1,11 @@
 package com.example.myreaderapp.components
 
 import android.os.Build
+import android.view.MotionEvent
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -35,13 +39,20 @@ import androidx.compose.material.icons.filled.StarBorder
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -56,6 +67,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
+import com.example.myreaderapp.R
 import com.example.myreaderapp.model.MBook
 import com.example.myreaderapp.navigation.ReaderScreens
 import com.example.myreaderapp.ui.theme.BerkshireSwash
@@ -328,5 +340,58 @@ fun FABContent(onTap: () -> Unit) {
             tint = Color.White)
     }
 }
+
+//Rating Bar
+@ExperimentalComposeUiApi
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Int,
+    onPressRating: (Int) -> Unit
+) {
+    var ratingState by remember {
+        mutableStateOf(rating)
+    }
+
+    var selected by remember {
+        mutableStateOf(false)
+    }
+    val size by animateDpAsState(
+        targetValue = if (selected) 42.dp else 34.dp,
+        spring(Spring.DampingRatioMediumBouncy)
+
+    )
+
+    Row(
+        modifier = Modifier.width(280.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        for (i in 1..5) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_baseline_star_24),
+                contentDescription = "star",
+                modifier = modifier
+                    .width(size)
+                    .height(size)
+                    .pointerInteropFilter {
+                        when (it.action) {
+                            MotionEvent.ACTION_DOWN -> {
+                                selected = true
+                                onPressRating(i)
+                                ratingState = i
+                            }
+                            MotionEvent.ACTION_UP -> {
+                                selected = false
+                            }
+                        }
+                        true
+                    },
+                tint = if (i <= ratingState) Color(0xFFFFD700) else Color(0xFFA2ADB1)
+            )
+        }
+    }
+}
+
 
 
--- app/src/main/java/com/example/myreaderapp/screens/update/ReaderBookUpdateScreen.kt
+++ app/src/main/java/com/example/myreaderapp/screens/update/ReaderBookUpdateScreen.kt
@@ -2,6 +2,7 @@ package com.example.myreaderapp.screens.update
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,25 +15,37 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Card
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
+import com.example.myreaderapp.components.InputField
+import com.example.myreaderapp.components.RatingBar
 import com.example.myreaderapp.components.ReaderAppBar
 import com.example.myreaderapp.data.DataOrException
 import com.example.myreaderapp.screens.home.HomeScreenViewModel
@@ -50,7 +63,7 @@ fun BookUpdateScreen(navController: NavController,
             icon = Icons.Default.ArrowBack,
             showProfile = false,
             navController = navController){
-            //navController.popBackStack()
+            navController.popBackStack()
         }
     }) {
 
@@ -75,40 +88,161 @@ fun BookUpdateScreen(navController: NavController,
                     Surface(modifier = Modifier
                         .padding(2.dp)
                         .fillMaxWidth(),
-                        shape = CircleShape,
+                        shape = RoundedCornerShape(20.dp),
                         elevation = 4.dp) {
 
                         ShowBookUpdate(bookInfo = viewModel.data.value, bookItemId = bookItemId)
                     }
+
+                    ShowSimpleForm(book = viewModel.data.value.data?.first { mBook ->
+                        mBook.googleBookId == bookItemId
+                    }!!, navController)
+
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun ShowBookUpdate(bookInfo: DataOrException<List<MBook>, Boolean, Exception>, bookItemId: String) {
+fun ShowSimpleForm(book: MBook,
+                   navController: NavController) {
+
+    val notesText = remember {
+        mutableStateOf("")
+    }
+
+    val isStartedReading = remember {
+        mutableStateOf(false)
+    }
+
+    val isFinishedReading = remember {
+        mutableStateOf(false)
+    }
+
+    val ratingVal = remember{
+        mutableStateOf(0)
+    }
+
+    SimpleForm(defaultValue = book.notes.toString().ifEmpty { "No thoughts available." }) { note ->
+        notesText.value = note
+    }
+
+    Row(
+        modifier = Modifier.padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        TextButton(
+            onClick = { isStartedReading.value = true },
+            enabled = book.startedReading == null
+        ) {
+            if (book.startedReading == null) {
+                if (!isStartedReading.value) {
+                    Text(text = "Start Reading")
+                } else {
+                    Text(
+                        text = "Reading in progress",
+                        modifier = Modifier.alpha(0.6f),
+                        color = Color.Red.copy(alpha = 0.5f)
+                    )
+                }
+
+            } else {
+                Text(text = "Started on: ${book.startedReading}") //TODO: format date
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            TextButton(
+                onClick = { isFinishedReading.value = true },
+                enabled = book.finishedReading == null
+            ) {
+                if (book.finishedReading == null) {
+                    if (!isFinishedReading.value) {
+                        Text(text = "Mark as read")
+                    } else {
+                        Text(
+                            text = "Finished reading",
+                            modifier = Modifier.alpha(0.6f),
+                            color = Color.Red.copy(alpha = 0.5f)
+                        )
+                    }
+
+                } else {
+                    Text(text = "Finished on: ${book.startedReading}") //TODO: format date
+                }
+
+            }
+        }
+    }
+    Text(text = "Rating", modifier = Modifier.padding(bottom = 3.dp))
+    book.rating?.toInt().let {
+        RatingBar(rating = it!!){rating ->
+            ratingVal.value = rating
+        }
+
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SimpleForm(
+        modifier: Modifier = Modifier,
+        loading: Boolean = false,
+        defaultValue: String = "Great Book!",
+        onSearch: (String) -> Unit
+    ) {
+    Column {
+        val textFieldValue = rememberSaveable { mutableStateOf(defaultValue) }
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val valid = remember(textFieldValue.value) { textFieldValue.value.trim().isNotEmpty() }
+
+        InputField(
+            modifier
+                .fillMaxWidth()
+                .height(140.dp)
+                .padding(3.dp)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp),
+            valueState = textFieldValue,
+            lableId = "Enter Your thoughts",
+            enabled = true,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onSearch(textFieldValue.value.trim())
+                keyboardController?.hide()
+            })
+
+    }
+}
+
+
+@Composable
+fun ShowBookUpdate(
+        bookInfo: DataOrException<List<MBook>, Boolean, Exception>,
+        bookItemId: String
+    ) {
     Row() {
         Spacer(modifier = Modifier.width(43.dp))
-        if(bookInfo.data != null){
-            Column(modifier = Modifier.padding(4.dp),
-            verticalArrangement = Arrangement.Center) {
+        if (bookInfo.data != null) {
+            Column(
+                modifier = Modifier.padding(4.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
 
                 CardListItem(book = bookInfo.data!!.first { mBook ->
                     mBook.googleBookId == bookItemId
-                }, onPressDetails = {
-                    hashCode()
-                })
+                }, onPressDetails = {})
             }
 
         }
-        
-    }
 
+    }
 }
 
+
 @Composable
-fun CardListItem(book: MBook, onPressDetails: () -> Unit){
+fun CardListItem(book: MBook, onPressDetails: () -> Unit) {
     Card(
         modifier = Modifier
             .padding(
@@ -121,45 +255,50 @@ fun CardListItem(book: MBook, onPressDetails: () -> Unit){
             .clickable { },
         elevation = 8.dp
     ) {
-
-        Image(
-            painter = rememberImagePainter(data = book.photoUrl.toString()),
-            contentDescription = null,
-            modifier = Modifier
-                .height(100.dp)
-                .width(120.dp)
-                .padding(4.dp)
-                .clip(
-                    RoundedCornerShape(
-                        topStart = 120.dp,
-                        topEnd = 20.dp,
-                        bottomEnd = 0.dp,
-                        bottomStart = 0.dp
+        Row(horizontalArrangement = Arrangement.Start) {
+            Image(
+                painter = rememberImagePainter(data = book.photoUrl.toString()),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(120.dp)
+                    .padding(4.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 120.dp,
+                            topEnd = 20.dp,
+                            bottomEnd = 0.dp,
+                            bottomStart = 0.dp
+                        )
                     )
+            )
+
+            Column {
+                Text(
+                    text = book.title.toString(),
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .width(120.dp),
+                    fontWeight = FontWeight.SemiBold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
-        )
 
-        Column {
-            Text(
-                text = book.title.toString(),
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier
-                    .padding(start = 8.dp, end = 8.dp)
-                    .width(120.dp),
-                fontWeight = FontWeight.SemiBold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
+                Text(
+                    text = book.authors.toString(),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, top = 2.dp, bottom = 0.dp)
+                )
 
-            Text(
-                text = book.publishedDate.toString(),
-                style = MaterialTheme.typography.h2,
-                modifier = Modifier
-                    .padding(start = 8.dp, end = 8.dp, top = 0.dp, bottom = 8.dp)
-            )
-
+                Text(
+                    text = book.publishedDate.toString(),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, top = 0.dp, bottom = 8.dp)
+                )
+            }
         }
-
-
     }
 }
